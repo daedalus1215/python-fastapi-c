80b9c8dc69bdc8520877a8af5a985ff7bdc450fd
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-02 11:18:18 -0500

    add optional to description field

--- books2.py
+++ books2.py
@@ -1,22 +1,27 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from uuid import UUID
 
-
 app = FastAPI()
+
 
 class Book(BaseModel):
     id: UUID
     title: str = Field(min_length=12)
     author: str
-    description: str
+    description: Optional[str] = Field(title="Description of the book", max_lengh=100, min_length=1)
     rating: int
 
+
 BOOKS = []
+
 
 @app.get("/")
 async def read_all_books():
     return BOOKS
+
 
 @app.post("/")
 async def create_book(book: Book):
