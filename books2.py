469209c1a7eb7b4c3432043b477a07b3b5e6caa1
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-02 15:59:13 -0500

    Response Model

--- books2.py
+++ books2.py
@@ -1,8 +1,16 @@
 from typing import Optional
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel, Field
 from uuid import UUID
+
+from starlette.responses import JSONResponse
+
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
+
 
 app = FastAPI()
 
@@ -26,11 +34,32 @@ class Book(BaseModel):
         }
 
 
+class BookNoRating(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str
+    description: Optional[str] = Field(None,
+                                       title="description of the Book",
+                                       max_length=100,
+                                       min_length=1
+                                       )
+
+
 BOOKS = []
+
+
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request, exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Hey, why do you want {exception.books_to_return} books? You need blah blah!"}
+    )
 
 
 @app.get("/")
 async def read_all_books(books_to_return: Optional[int] = None):
+    if books_to_return and books_to_return < 0:
+        raise NegativeNumberException(books_to_return=books_to_return)
     if len(BOOKS) < 1:
         create_books_no_api()
     if books_to_return and len(BOOKS) >= books_to_return > 0:
@@ -41,6 +70,22 @@ async def read_all_books(books_to_return: Optional[int] = None):
             i += 1
         return new_books
     return BOOKS
+
+
+@app.get("/book/{book_id}")
+async def read_book(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+    raise raise_item_cannot_be_found_exception()
+
+
+@app.get("/book/rating/{book_id}", response_model=BookNoRating)
+async def read_book_no_rating(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+    raise raise_item_cannot_be_found_exception()
 
 
 @app.post("/")
@@ -62,7 +107,7 @@ async def update_book(book_id: UUID, book: Book):
 
 @app.delete("/{book_id}")
 async def delete_book(book_id: UUID):
-    counter = 0;
+    counter = 0
     for x in BOOKS:
         counter + 1
         if x.id == book_id:
@@ -74,7 +119,7 @@ async def delete_book(book_id: UUID):
 def create_books_no_api():
     book_1 = Book(id="3fa85f64-5717-4562-b3fc-2c963f66afa6", title="title 1, has to be at least 12",
                   author="author", description="description", rating=50)
-    BOOKS.append(book_1);
+    BOOKS.append(book_1)
 
 
 def raise_item_cannot_be_found_exception():
