5fc93841dff6f15a9908c611f1d9119526bf9c8a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-03 11:12:09 -0500

    hash a password

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
 from pydantic import BaseModel
 from typing import Optional
 import models
@@ -12,7 +13,13 @@ class CreateUser(BaseModel):
     password: str
 
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 app = FastAPI()
+
+
+def get_password_has(password):
+    return bcrypt_context.hash(password)
 
 
 @app.post("/create/user")
@@ -22,8 +29,7 @@ async def create_new_user(create_user: CreateUser):
     create_user_model.username = create_user.username
     create_user_model.first_name = create_user.first_name
     create_user_model.last_name = create_user.last_name
-    create_user_model.hashed_password = create_user.password
+    create_user_model.hashed_password = get_password_has(create_user.password)
     create_user_model.is_active = True
 
     return create_user_model
-
