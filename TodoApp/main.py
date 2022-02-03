4b3ebecf845fefa94fb8364e4f22bf0509752f4b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-03 15:53:43 -0500

    abstract into multiple routers

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,96 +1,16 @@
-from typing import Optional
-from fastapi import FastAPI, Depends, HTTPException
 import models
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
-from auth import router
+import auth
+import todos
+from fastapi import FastAPI
+from database import engine
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(router)
-
-def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
+app.include_router(auth.router)
+app.include_router(todos.router)
 
 
-class Todo(BaseModel):
-    title: str
-    description: Optional[str]
-    priority: int = Field(gt=0, lt=6, description="priority must be between 1-5")
-    complete: bool
 
 
-@app.get("/")
-async def read_all(db: Session = Depends(get_db)):
-    return db.query(models.Todos).all()
-
-
-@app.get("/todo/{todo_id}")
-async def read_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
-    if todo_model is not None:
-        return todo_model
-    http_exception()
-
-
-@app.post("/")
-async def create_todo(todo: Todo, db: Session = Depends(get_db)):
-    todo_model = models.Todos()
-    todo_model.title = todo.title
-    todo_model.description = todo.description
-    todo_model.priority = todo.priority
-    todo_model.complete = todo.complete
-
-    db.add(todo_model)
-    db.commit()
-
-    return successful_response(200)
-
-
-@app.put("/{todo_id}")
-async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
-
-    if todo_model is None:
-        http_exception()
-
-    todo_model.title = todo.title
-    todo_model.description = todo.description
-    todo_model.priority = todo.priority
-    todo_model.complete = todo.complete
-
-    db.add(todo_model)
-    db.commit()
-
-    return successful_response(200)
-
-
-@app.delete("/{todo_id}")
-async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
-
-    if todo_model is None:
-        http_exception()
-
-    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
-    db.commit()
-
-    return successful_response(200)
-
-
-def http_exception():
-    raise HTTPException(status_code=404, detail="Todo not found")
-
-
-def successful_response(status_code: int):
-    return {
-        'status': status_code,
-        'transaction': 'Successful'
-    }
---
+++ TodoApp/todos.py
@@ -0,0 +1,90 @@
+from database import engine, SessionLocal
+import models
+from typing import Optional
+from fastapi import Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+
+router = APIRouter()
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="priority must be between 1-5")
+    complete: bool
+
+
+@router.get("/")
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
+
+
+@router.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    http_exception()
+
+
+@router.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return successful_response(200)
+
+
+@router.put("/{todo_id}")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        http_exception()
+
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return successful_response(200)
+
+
+@router.delete("/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        http_exception()
+
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    db.commit()
+
+    return successful_response(200)
+
+
+def http_exception():
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+
+def successful_response(status_code: int):
+    return {
+        'status': status_code,
+        'transaction': 'Successful'
+    }
