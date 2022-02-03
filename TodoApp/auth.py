83d3408cfa7ace3dc5e6d02e26a500dc96b3d02d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-03 14:37:12 -0500

    we now return token

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -5,7 +5,13 @@ from typing import Optional
 import models
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = 'KlgH6AzYDeZeGwD288to89282kdjfjfkdw'
+ALGORITHM = "HS256"
+
 
 class CreateUser(BaseModel):
     username: str
@@ -19,7 +25,10 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
 
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+
 app = FastAPI()
+
 
 def get_db():
     try:
@@ -28,6 +37,7 @@ def get_db():
     finally:
         db.close()
 
+
 def get_password_has(password):
     return bcrypt_context.hash(password)
 
@@ -35,7 +45,8 @@ def get_password_has(password):
 def verify_password(plain_password, hashed_password):
     return bcrypt_context.verify(plain_password, hashed_password)
 
-def authenticate_user(username:str, password:str, db):
+
+def authenticate_user(username: str, password: str, db):
     user = db.query(models.Users).filter(models.Users.username == username).first()
 
     if not user:
@@ -43,6 +54,17 @@ def authenticate_user(username:str, password:str, db):
     if not verify_password(password, user.hashed_password):
         return False
     return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
+    encode = {"sub": username, "id": user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({"exp": expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
 
 @app.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
@@ -59,9 +81,12 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
 
     return create_user_model
 
+
 @app.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    return "User Validated"
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(user.username, user.id, expires_delta=token_expires)
+    return token
