44b61f087e8338bfd453cb40e55fda1e9774f35f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-03 09:52:15 -0500

    abstract exception and use abstraction

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -15,13 +15,19 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
 
 @app.get("/todo/{todo_id}")
 async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
     if todo_model is not None:
         return todo_model
+    http_exception()
+
+
+def http_exception():
     raise HTTPException(status_code=404, detail="Todo not found")
