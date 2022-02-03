b4e18e02c5f58e1b2f7fd8738cc8f8d07d3e233d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-03 14:21:27 -0500

    create user RH

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from typing import Optional
 import models
-
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
 
 class CreateUser(BaseModel):
     username: str
@@ -15,15 +16,23 @@ class CreateUser(BaseModel):
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 def get_password_has(password):
     return bcrypt_context.hash(password)
 
 
 @app.post("/create/user")
-async def create_new_user(create_user: CreateUser):
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
     create_user_model.username = create_user.username
@@ -31,5 +40,8 @@ async def create_new_user(create_user: CreateUser):
     create_user_model.last_name = create_user.last_name
     create_user_model.hashed_password = get_password_has(create_user.password)
     create_user_model.is_active = True
+
+    db.add(create_user_model)
+    db.commit()
 
     return create_user_model
